1c54033e6bbcc7730006367dc645f392ce07150d
abhidatta0 <abhidatta146@gmail.com>
2025-11-24 10:47:54 +0530

    refactored folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { Fragment, useState } from "react";
-import Accordion from "./components/Accordion";
-import data from "./data";
+import AccordionExample from "./components/Accordion/Example";
 
 function App() {
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   return (
-    <div className="p-3">
-      {data.map((qa, index) => (
-        <Fragment key={index}>
-          <Accordion
-            isOpen={selectedIndex === index}
-            header={qa.question}
-            content={qa.answer}
-            onClick={() => setSelectedIndex(index)}
-          />
-          <div className=" border-t border-[#D9DBE9]"></div>
-        </Fragment>
-      ))}
-    </div>
+    <AccordionExample />
   );
 }
 
---
+++ src/components/Accordion/Example.tsx
@@ -0,0 +1,24 @@
+import { Fragment, useState } from "react";
+import data from "../../data";
+import Accordion from "./Accordion";
+
+const Example = () => {
+    const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  
+  return (
+   <div className="p-3">
+      {data.map((qa, index) => (
+        <Fragment key={index}>
+          <Accordion
+            isOpen={selectedIndex === index}
+            header={qa.question}
+            content={qa.answer}
+            onClick={() => setSelectedIndex(index)}
+          />
+          <div className=" border-t border-[#D9DBE9]"></div>
+        </Fragment>
+      ))}
+    </div>
+  )
+}
+export default Example
